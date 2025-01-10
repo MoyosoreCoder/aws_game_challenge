cd5a5f65b539af49f7391dd89a1cd45d14ebc330
Mariam <kelanimaryam1234@gmail.com>
2025-01-10 02:51:36 +0100

    edited the App.js code

--- brain-game/src/App.js
+++ brain-game/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <h1>Welcome!</h1>
       <button>Click the button to play game</button>
     </div>
   );
